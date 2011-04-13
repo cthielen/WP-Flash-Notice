3b347b1bf01a534484e3685c1856a749fde3ba8a
Christopher Thielen <chris@epiar.net>
2011-04-13 09:14:09 -0700

    Initial concept finished and working.

--- flash-notice.php
+++ flash-notice.php
@@ -1,82 +1,60 @@
 <?php
 /**
- * @package Hello_Dolly
- * @version 1.6
+ * @package Flash_Notice
+ * @version 0.1
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.6
-Author URI: http://ma.tt/
+Plugin Name: Flash Notice
+Plugin URI: 
+Description: Displays a notice in the template where desired. Notice can be string or HTML.
+Author: Christopher Thielen
+Version: 0.1
+Author URI: http://christopherthielen.com/
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
+// Set the default message to NULL
+add_option('wp-flash-notice-msg', "");
 
-	// Here we split it into lines
-	$lyrics = explode( "\n", $lyrics );
-
-	// And then randomly choose a line
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+// Template tag function to be called from any template
+function flash_notice_tag() {
+	echo get_option('wp-flash-notice-msg');
 }
 
-// This just echoes the chosen line, we'll position it later
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>$chosen</p>";
+// Set up the admin panel menu
+add_action('admin_menu', 'wp_flash_notice_create_menu');
+
+function wp_flash_notice_create_menu() {
+	// create new top-level menu
+	add_menu_page('Flash Notice Settings', 'Flash Notice Settings', 'administrator', __FILE__, 'flash_notice_settings_page');
+
+	// call register settings function
+	add_action( 'admin_init', 'register_flash_notice_settings' );
 }
 
-// Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hello_dolly' );
 
-// We need some CSS to position the paragraph
-function dolly_css() {
-	// This makes sure that the positioning is also good for right-to-left languages
-	$x = is_rtl() ? 'left' : 'right';
-
-	echo "
-	<style type='text/css'>
-	#dolly {
-		float: $x;
-		padding-$x: 15px;
-		padding-top: 5px;		
-		margin: 0;
-		font-size: 11px;
-	}
-	</style>
-	";
+function register_flash_notice_settings() {
+	register_setting( 'wp-flash-notice-settings-group', 'wp-flash-notice-msg' );
 }
 
-add_action( 'admin_head', 'dolly_css' );
-
+function flash_notice_settings_page() {
 ?>
+<div class="wrap">
+<h2>Flash Notice</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'wp-flash-notice-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Flash Notice</th>
+        <td><textarea name="wp-flash-notice-msg" rows="8" cols="50"><?php echo get_option('wp-flash-notice-msg'); ?></textarea><br /><i>Set as blank to disable.</i></td>
+        </tr>
+    </table>
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+
+</form>
+</div>
+
+<?php } ?>
